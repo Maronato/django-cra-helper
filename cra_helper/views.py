274aa4e7d42592ae867823ca87bcc9ed86ffbb7a
Maronato <gustavomaronato@gmail.com>
2020-11-29 18:14:04 -0300

    use server cra url with proxies

--- cra_helper/views.py
+++ cra_helper/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from proxy.views import proxy_view
 
-from cra_helper import CRA_URL
+from cra_helper import CRA_SERVER_URL
 
 
 @csrf_exempt
@@ -16,5 +16,5 @@ def proxy_cra_requests(request, path):
   re_path(r'^__webpack_dev_server__/(?P<path>.*)$', proxy_sockjs),
   '''
   path = request.path
-  url = '{}{}'.format(CRA_URL, path)
+  url = '{}{}'.format(CRA_SERVER_URL, path)
   return proxy_view(request, url)
